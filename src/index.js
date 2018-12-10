8a44a59ce75384757725b6cac972091a182ef96b
Josh Poliquin <jshpoliquin@gmail.com>
2018-12-10 16:00:41 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <App name = {'Dr. Seuss'} />,
+  document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+// Stops page from refreshing. However, still re-renders components
+if (module.hot){
+  module.hot.accept();
+}
